d9c50b2b5abca4f6d9482d9f263eb2329bbe9ce1
Your Name <you@example.com>
2023-09-15 13:06:51 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+function solace(){
+  alert("message received");
+}
